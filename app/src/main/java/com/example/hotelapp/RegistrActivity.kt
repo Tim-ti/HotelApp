7bfa514d10aa4eef2ceb2c5041722a892a6bfbab
Tsimafey <timtarl59@gmail.com>
2023-05-10 13:45:40 +0300

    try to do save to DB

--- app/src/main/java/com/example/hotelapp/RegistrActivity.kt
+++ app/src/main/java/com/example/hotelapp/RegistrActivity.kt
@@ -1,8 +1,10 @@
 package com.example.hotelapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hotelapp.databinding.ActivityMainBinding
+import com.example.hotelapp.databinding.ActivityRegistrBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class RegistrActivity : AppCompatActivity() {
@@ -12,6 +14,22 @@ class RegistrActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registr)
+        val binding = ActivityRegistrBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener{
+            startActivity(Intent(this,SignInActivity::class.java))
+            finish()
+        }
+        binding.btnRegister.setOnClickListener{
+            val email = binding.emailField.text.toString()
+            val password = binding.passField.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty())
+                MainActivity.auth.createUserWithEmailAndPassword(email, password).addOnCanceledListener {
+                    startActivity(Intent(this,MainActivity::class.java))
+                    finish()
+                }
+        }
+
     }
 }
