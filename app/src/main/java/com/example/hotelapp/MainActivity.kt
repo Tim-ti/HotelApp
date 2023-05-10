bcc2d25b7240149457e6acb1658b60a2df7443ad
Tsimafey <timtarl59@gmail.com>
2023-05-10 13:07:06 +0300

    made a signIn button

--- app/src/main/java/com/example/hotelapp/MainActivity.kt
+++ app/src/main/java/com/example/hotelapp/MainActivity.kt
@@ -4,17 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
+import com.example.hotelapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 public class MainActivity : AppCompatActivity() {
     private lateinit var btnSignIn: Button
     private lateinit var btnRegister: Button
-
+    private lateinit var binding: ActivityMainBinding
+    companion object{
+            lateinit var auth: FirebaseAuth
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener{
+            startActivity(Intent(this,SignInActivity::class.java))
+            finish()
+        }
 
         btnSignIn = findViewById(R.id.btnSignIn)
         btnRegister = findViewById(R.id.btnRegister)
--- app/src/main/java/com/example/hotelapp/RegistrActivity.kt
+++ app/src/main/java/com/example/hotelapp/RegistrActivity.kt
@@ -2,12 +2,13 @@ package com.example.hotelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hotelapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class RegistrActivity : AppCompatActivity() {
 
         private lateinit var auth: FirebaseAuth
-        private lateinit var binding: ActivitySignUpBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
