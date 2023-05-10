b51a55ce82abb8cbae30ad0e2e69ada117daa70c
Tsimafey <timtarl59@gmail.com>
2023-05-10 12:04:32 +0300

    test push

--- app/src/main/java/com/example/hotelapp/RegistrActivity.kt
+++ app/src/main/java/com/example/hotelapp/RegistrActivity.kt
@@ -2,8 +2,13 @@ package com.example.hotelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrActivity : AppCompatActivity() {
+
+        private lateinit var auth: FirebaseAuth
+        private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registr)
